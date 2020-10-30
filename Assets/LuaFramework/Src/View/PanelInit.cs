b960faf181318c83a246d546ee492b0f951d6649
uncle <1922459087@qq.com>
2020-10-30 11:11:14 +0800

    PanelInit.cs

---
+++ Assets/LuaFramework/Src/View/PanelInit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaFramework
+{
+    /// <summary>
+    /// 面板id
+    /// </summary>
+    public enum PanelId: uint
+    {
+        Panel_NULL = 0,
+        Panel_Test = 1,//测试面板
+    }
+
+    [LuaFrameworkPanel(PanelId.Panel_Test, "UI/Prefabs/View/UILogin.prefab")]
+    public class TestPanel : View
+    {
+
+    }
+
+}
